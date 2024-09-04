f702309023a7c8ebea28fe3846c52eda245bf0da
Janagan <163739252+Janaganmk@users.noreply.github.com>
2024-09-04 21:16:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var wedcake=document.getElementById("wed1")
+var wedcake2=document.getElementById("wed2")
+var wedcake3=document.getElementById("wed3")
+
+function wed(){
+    wedcake.style.display="block"
+    wedcake2.style.display="block"
+    wedcake3.style.display="block"
+}
